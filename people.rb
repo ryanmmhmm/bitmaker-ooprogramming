9604ed8436a000b7e6e3e6a25bb5e02d63c50294
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-29 14:21:07 -0400

    added init method to parent class Person

--- people.rb
+++ people.rb
@@ -1,16 +1,18 @@
 ## Exercise 1 -- Week 1 Day 4
 
 class Person
-
+	def initialize(name)
+		@name = name
+	end
 end
 
-class	Student
+class	Student < Person
 	def learn
 		puts "I get it!"
 	end
 end
 
-class	Instructor
+class	Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object."
 	end
